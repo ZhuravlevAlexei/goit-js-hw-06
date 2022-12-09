4eab6c75d92ee820999fe513655dbebf0a42d568
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2022-12-09 20:40:53 +0200

    current changes

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingList = document.getElementById("ingredients");
+
+const arrayLi = [];
+ingredients.forEach((elm) => {
+  const newLi = document.createElement("li");
+  newLi.textContent = elm;
+  newLi.classList.add("item");
+  arrayLi.push(newLi);
+});
+
+ingList.before(...arrayLi);
