4c636c2be9e36c235acac6ec3c7bd2e4f473daee
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2022-12-11 09:27:45 +0200

    All tasks

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const mySpan = document.querySelector(".color");
+const myButton = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onButtonClick(evt) {
+  const documentBody = document.body;
+
+  const randomColor = getRandomHexColor();
+  documentBody.style.backgroundColor = randomColor;
+  mySpan.textContent = randomColor;
+}
+
+myButton.addEventListener("click", onButtonClick);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,49 @@
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const divControls = document.querySelector("#controls");
+const dataInput = divControls.firstElementChild;
+const divBoxes = document.querySelector("#boxes");
+const size = 30;
+
+divBoxes.style.display = "flex";
+divBoxes.style.flexWrap = "wrap";
+divBoxes.style.justifyContent = "center";
+divBoxes.style.alignItems = "center";
+divBoxes.style.gap = "10px";
+// divBoxes.style.width = "50px";
+// divBoxes.style.height = "50px";
+// divBoxes.style.backgroundColor = "black";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(evt) {
+  destroyBoxes();
+  let curSize = size;
+  const amount = dataInput.value;
+
+  const arrayDivs = [];
+  const emptyChild = document.createElement("div");
+  divBoxes.appendChild(emptyChild);
+
+  for (let idx = 1; idx <= amount; idx += 1) {
+    let newEl = document.createElement("div");
+    newEl.style.width = `${curSize}px`;
+    newEl.style.height = `${curSize}px`;
+    newEl.style.backgroundColor = getRandomHexColor();
+    arrayDivs.push(newEl);
+    curSize += 10;
+  }
+  emptyChild.before(...arrayDivs);
+}
+
+function destroyBoxes(evt) {
+  divBoxes.innerHTML = "";
+}
+
+createButton.addEventListener("click", createBoxes);
+
+destroyButton.addEventListener("click", destroyBoxes);
+
+//console.log(divBoxes);
